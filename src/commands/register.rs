2dddab82bd7626353d380c38de0908f1aef35332
Caden32 <github@caden32.com>
2024-03-20 17:30:56 -0600

    Replace voting system with registration feature

--- src/commands/register.rs
+++ src/commands/register.rs
@@ -1,12 +1,47 @@
-use crate::{Context, Error};
+use crate::{Context, Error, Data};
+use poise::serenity_prelude as serenity;
+
+#[derive(Debug, poise::Modal)]
+struct RegisterModal {
+    first_name: String,
+    last_initial: Option<String>,
+    student_email: String
+}
 #[poise::command(slash_command, ephemeral)]
 pub async fn register(
-    ctx: Context<'_>,
-    name: String,
-    s_number: String,
-    student_email: String,
+    ctx: poise::ApplicationContext<'_, Data, Error>,
 ) -> Result<(), Error> {
+    use poise::Modal as _;
+    let data = RegisterModal::execute(ctx).await?;
+    println!("Got data; {:?}", data);
     ctx.say("Register command reached").await?;
-    ctx.say(format!("Name:{name}\nSNumber{s_number}\nStudentEmail\n{student_email}")).await?;
+    Ok(())
+}
+
+#[poise::command(slash_command)]
+pub async fn component_modal(ctx: Context<'_>) -> Result<(), Error> {
+    let reply = {
+        let components = vec![serenity::CreateActionRow::Buttons(vec![
+            serenity::CreateButton::new("open_modal")
+                .label("Open modal")
+                .style(serenity::ButtonStyle::Success),
+        ])];
+
+        poise::CreateReply::default()
+            .content("Click the button below to open the modal")
+            .components(components)
+    };
+
+    ctx.send(reply).await?;
+
+    while let Some(mci) = serenity::ComponentInteractionCollector::new(ctx.serenity_context())
+        .timeout(std::time::Duration::from_secs(120))
+        .filter(move |mci| mci.data.custom_id == "open_modal")
+        .await
+    {
+        let data =
+            poise::execute_modal_on_component_interaction::<RegisterModal>(ctx, mci, None, None).await?;
+        println!("Got data: {:?}", data);
+    }
     Ok(())
 }
